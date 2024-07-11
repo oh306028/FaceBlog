834dd025a606be54a3a294176fcf8ceab3991423
Oskar <hamerlao@gmail.com>
2024-07-11 23:51:22 +0200

    Login and Register viewModels added

---
+++ App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }        
+    }
+}
---
+++ App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords have to be equal")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required]
+        public DateTime DateOfbirth { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }  
+
+    }
+}
