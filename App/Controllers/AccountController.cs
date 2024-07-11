24b590b438da11c20739ae05e5b0eb694660c8be
Oskar <hamerlao@gmail.com>
2024-07-11 23:50:58 +0200

    Account contorller added

---
+++ App/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using App.Models;
+using App.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager; 
+
+        }
+
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return View(model);
+        }
+
+
+
+  
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+
+
+            return View(model);
+        }
+
+
+
+     
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+    }
+}
