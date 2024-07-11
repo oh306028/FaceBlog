53c5f4b9ea67cb20649f261e7c204523c6bc6aa5
Oskar <hamerlao@gmail.com>
2024-07-12 00:52:07 +0200

    Migration connected with AddressId added

---
+++ App/Migrations/20240711222339_addressIdAdded.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class addressIdAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+              name: "AddressId",
+              table: "AspNetUsers",
+              type: "int",
+              nullable: true);
+
+
+            migrationBuilder.CreateIndex(
+               name: "IX_AspNetUsers_AddressId",
+               table: "AspNetUsers",
+               column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+    name: "FK_AspNetUsers_Addresses_AddressId",
+    table: "AspNetUsers",
+    column: "AddressId",
+    principalTable: "Addresses",
+    principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+  name: "IX_AspNetUsers_AddressId",
+  table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+    name: "FK_AspNetUsers_Addresses_AddressId",
+    table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+             name: "AddressId",
+             table: "AspNetUsers");
+
+        }
+    }
+}
