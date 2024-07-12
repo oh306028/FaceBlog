ecca5d297adc73d6298b8862c2d35b051e82107f
Oskar <hamerlao@gmail.com>
2024-07-12 22:12:35 +0200

    update view model add

---
+++ App/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class UpdateViewModel
+    {
+       
+        public string? FirstName { get; set; }
+ 
+        public string? LastName { get; set; }
+
+
+
+        //most important data
+        public string? UserName { get; set; }
+
+       
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+       
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+     
+    
+        
+
+
+        public DateTime? DateOfbirth { get; set; }
+
+
+        public string? Country { get; set; }        
+        public string? City { get; set; }       
+        public string? Street { get; set; }
+    }
+}
