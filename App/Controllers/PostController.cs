ae7ccc26b5ebf89f7ca86dc78e7a98c8f18c5356
Oskar <hamerlao@gmail.com>
2024-07-13 23:12:54 +0200

    Post controller added

---
+++ App/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly BlogDbContext _context;
+
+        public PostController(BlogDbContext context)
+        {
+            _context = context;
+        }       
+
+        public IActionResult Index()
+        {
+            var posts = _context.Posts.Include(u => u.Createdby).ThenInclude(c => c.Comments).ToList(); 
+                    
+            return View(posts);
+        }
+
+
+
+
+    }
+}
